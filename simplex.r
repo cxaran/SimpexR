d650c76116a42629014a13a1944d674dfb70ffeb
cxaran <xxjmap@gmail.com>
2022-01-19 10:09:20 -0600

    Update simplex.r

--- simplex.r
+++ simplex.r
@@ -4,16 +4,14 @@ library("rjson")
 inst <- fromJSON(file = "instance.json")
 
 #guardamos el numero de variables
-vars <- inst$vars; vars
+vars <- inst$vars;
 
 #leer restricciones
-t<-matrix(t(unlist(inst$rest)),ncol=length(inst$rest),nrow=vars+2);t
+t<-matrix(t(unlist(inst$rest)),ncol=length(inst$rest),nrow=vars+2);
 sig<-t[vars+1,];
 t<-matrix(as.numeric(t(t[-(vars+1),])), ncol = ncol(t(t[-(vars+1),])));
 t<-cbind(t[,vars+1],t[,-(vars+1)]);
 
-#sing<-t[vars+1,];sing[sig=="<="] <- 1;sing[sig==">="] <- -1;
-#t<-t*as.numeric(sing);t;
 
 restricciones<-t[,-1]
 derecho<-t[,1]
@@ -39,10 +37,8 @@ t<-cbind(t,vall);
 
 #leer funcion objetivo
 objetivo<-unlist(inst$obj)
-#obj<-append(0,unlist(inst$obj))*-1;
-#obj<-append(as.numeric(obj),replicate(length(vall[1,]),0)) 
-#t<- rbind(obj,t);t
 t<-rbind(append(as.numeric(append(0,unlist(inst$obj))*-1),replicate(length(vall[1,]),0)),t);
+
 rbind(t[-1,],t[1,])
 
 #guardar
@@ -53,62 +49,52 @@ res[1]<-1
 f<-FALSE;
 fac<-TRUE
 
-library(lpSolve)
-o <- lp(inst$type,objetivo,restricciones,sig,derecho,all.int = T)
-
 #Metodo de dos fases
 if(length(va[va==1])>0  ){
   tx<-t;
   tx[1,]<-append(replicate(length(t[1,])-length(va[va==1]),0),replicate(length(va[va==1]),-1));
-  #tx<- cbind(tx, append(1,replicate( length(t[,1])-1,0 ))); tx
-  
   cs<- which(va==1);
   ts<- tx[cs+1,];
   ts<-rbind(ts,tx[1,])
   tx[1,]<-colSums(ts);
   tx[1,]<-tx[1,]*-1;
-  
-  #rbind(tx[-1,],tx[1,])
-  tx
-  
   res<-replicate(length(tx[1,]), 0)
   res[1]<-1
+  
+  print(rbind(tx[-1,],tx[1,]))
   
   #condicion de parada max
   f<-length(which(tx[1,-1]<0)) > 0
   while(f){
     #obtener pivote
     pa<-tx[1,-1]
-    pa <- (which(pa==min(pa)[1])[1])+1; tx[1,pa]; pa
+    pa <- (which(pa==min(pa)[1])[1])+1;
     pb <-tx[2:length(tx[,1]),pa];
     pc <- tx[2:length(tx[,1]),1];
-    pb[pb<=0]<-0;pb
-    pc[pb==0]<-1;pc
-    pb <-pc/pb;pb
-    pb <- which(pb==min(pb)[1])+1; tx[pb,pa];pb
+    pb[pb<=0]<-0;
+    pc[pb==0]<-1;
+    pb <-pc/pb;
+    pb <- (which(pb==min(pb)[1])[1])+1;
+    
+    print(tx[pb,pa])
     
     #guardar
-    res[which(res==pb)]=0
-    res[pa]<-pb
+    res[which(res==pb)]=0;
+    res[pa]<-pb;
     
     #Algortimo
-    tp <- as.numeric(tx[pb,]/tx[pb,pa]);tp
+    tp <- as.numeric(tx[pb,]/tx[pb,pa]);
     ty <- sweep(tx+tx[,pa]-tx,MARGIN=2,tp,`*`);
     tx <- tx-ty; 
     tx[pb,]<- tp; 
-    rbind(tx[-1,],tx[1,])
+    print(rbind(tx[-1,],tx[1,]))
     
     f<-length(which(tx[1,-1]<0)) > 0
   }
-  #tx
-  #no se si meter la z
+  #fase dos
   to<-t[1,]
   t<-tx[, 1 : (length(tx[1,])-length(va[va==1])) ]
   t[1,]<-to[1:length(t[1,])];
-  #t<-cbind(t,append(0,matrix(0, nrow =  length(t[,1])))); 
-  #t[1,length(t[1,])]<-1;
-  t
-  
   tm<-(-t[1,]);tm
   tm[res==0]<-0
   tm[tm<0]<-0
@@ -128,7 +114,6 @@ rbind(t[-1,],t[1,])
 
 if(fac & inst$type =="max")f<-length(which(t[1,-1]<0)) > 0
 if(fac & inst$type =="min")f<-length(which(t[1,-1]>0)) > 0
-f
 
 #Metodo simplex
 while(f){
@@ -136,13 +121,12 @@ while(f){
   pa<-t[1,]
   if(inst$type =="max") pa <- which(pa==min(pa))[1];
   if(inst$type =="min") pa <- which(pa==max(pa))[1];
-  t[1,pa]
-  pb <-t[2:length(tx[,1]),pa];pb
-  pc <- t[2:length(tx[,1]),1];pc
+  pb <-t[2:length(tx[,1]),pa];
+  pc <- t[2:length(tx[,1]),1];
   pb[pb<=0]<-0;
   pc[pb<=0]<-1;
   pb <-pc/pb;pb
-  pb <- (which(pb==min(pb))[1])+1; t[pb,pa]
+  pb <- (which(pb==min(pb)[1])[1])+1;
   
   print(t[pb,pa])
   
@@ -151,23 +135,21 @@ while(f){
   res[pa]<-pb
   
   #guardamos renglon
-  tp <- as.numeric(t[pb,]/t[pb,pa]); #tp
-  #renglon pivote por fila pivote
-  tx <- sweep(t+t[,pa]-t,MARGIN=2,tp,`*`); #tx
-  #restamos a tabla original
-  t  <- t-tx; #t
-  t[pb,]<- tp; #t
+  tp <- as.numeric(t[pb,]/t[pb,pa]);
+  tx <- sweep(t+t[,pa]-t,MARGIN=2,tp,`*`);
+  t  <- t-tx;
+  t[pb,]<- tp;
   print(rbind(t[-1,],t[1,]))
   
   if(inst$type =="max")f<-length(which(t[1,-1]<0)) > 0
   if(inst$type =="min")f<-length(which(t[1,-1]>0)) > 0
-  f
 }
-t
 res[which(res>0)]<-t[res,1];
 if(!fac) res<-0
 res[2:(vars+1)]
 res[1]
 
+library(lpSolve)
+o <- lp(inst$type,objetivo,restricciones,sig,derecho,all.int = T)
 o$solution
 o$objval
